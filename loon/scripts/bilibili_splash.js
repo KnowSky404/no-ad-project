409d376d40a6e78152233ff7d6022140c571bd45
knowsky404 <git@knowsky404.com>
2026-01-03 10:19:10 +0800

    feat: 更新开屏广告信息

--- loon/scripts/bilibili_splash.js
+++ loon/scripts/bilibili_splash.js
@@ -1,12 +1,13 @@
 /*
 脚本名称：Bilibili 开屏去广告
-接口路径：https://app.bilibili.com/x/v2/splash/list
+接口路径：
+1. https://app.bilibili.com/x/v2/splash/list
+2. https://app.bilibili.com/x/v2/splash/show
 功能：清空开屏广告缓存列表
 */
-// ^https:\/\/app\.bilibili\.com\/x\/v2\/splash\/list
 
 const url = $request.url;
-const body = $response.body;
+let body = $response.body;
 
 if (!body) {
     $done({});
@@ -19,24 +20,16 @@ try {
         // 核心逻辑：直接把广告列表清空
         // B站 App 读到空列表，就不会下载广告，下次启动直接进首页
 
+        // 1. 针对 /x/v2/splash/list 接口
         // 清空主要广告列表
         if (obj.data.list) {
             obj.data.list = [];
         }
-
         // 移除 keep_ids
         if (obj.data.keep_ids) {
-            obj.data.keep_ids = []
+            obj.data.keep_ids = [];
         }
-
-        // 清空展示控制列表
-        if (obj.data.show) {
-            obj.data.show = [];
-        }
-
-        // (可选) 修改一些时间参数，让App以为很久都不需要拉取新广告
-        // max_time: 广告展示最长时间
-        // min_interval: 下次拉取配置的最小间隔 (设为一年 31536000)
+        // 修改时间参数
         if (obj.data.max_time) {
             obj.data.max_time = 0;
         }
@@ -44,6 +37,12 @@ try {
             obj.data.pull_interval = 31536000;
         }
 
+        // 2. 针对 /x/v2/splash/show 接口 (以及 list 接口中的 show 字段)
+        // 清空展示控制列表
+        if (obj.data.show) {
+            obj.data.show = [];
+        }
+        
         console.log("[Bilibili] 开屏广告配置已清空");
     }
 
